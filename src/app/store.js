12a54cab7191dd760aac96f4dc3b9787dabacdc1
celina <celina.alvarezdiaz@bjss.com>
2022-04-07 16:48:13 +0100

    Add Posts State and Functionality

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import postsReducer from '../features/posts/postsSlice'
+import usersReducer from '../features/users/userSlice'
+
 export default configureStore({
-  reducer: () => ({}),
+  reducer: {
+    posts: postsReducer,
+    user: usersReducer
+  }
 })
+
+
