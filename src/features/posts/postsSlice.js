97c9f93aae28a0e4e26938cef8dedc259e7c3934
celina <celina.alvarezdiaz@bjss.com>
2022-04-08 13:46:01 +0100

    Fix reactionAdded in posts reducers

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -9,17 +9,17 @@ const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
+    reactionAdded(state,action){
+      const { postId, reaction} = action.payload
+      const existingPost = state.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    },
+    reducer(state, action) {
+      state.push(action.payload)
+    },
     postAdded: {
-      reactionAdded(state,action){
-        const { postId, reaction} = action.payload
-        const existingPost = state.find(post => post.id === postId)
-        if (existingPost) {
-          existingPost.reactions[reaction]++
-        }
-      },
-      reducer(state, action) {
-        state.push(action.payload)
-      },
       prepare(title, content, userId) {
         return {
           payload: {
