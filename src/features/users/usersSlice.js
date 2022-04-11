dae0113d29d08db57c63ce350bc2824561376109
celina <celina.alvarezdiaz@bjss.com>
2022-04-11 11:35:54 +0100

    Add userPage

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -23,3 +23,4 @@ export default usersSlice.reducer
 
 export const selectAllUsers = state => state.users
 
+export const selectUserById = (state, userId) => state.users.find(user => user.id === userId)
