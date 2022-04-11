bf04cee8f5145e1322e50848bed78032b1691e6b
celina <celina.alvarezdiaz@bjss.com>
2022-04-11 08:39:05 +0100

    Display fetched posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,25 +2,15 @@ import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { Spinner } from '../../components/Spinner'
 import { PostAuthor } from './PostAuthor'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
 
 import { selectAllPosts, fetchPosts } from './postsSlice'
 
-export const PostsList = () => {
-  const dispatch = useDispatch()
-  const posts = useSelector(selectAllPosts)
-
-  const postStatus = useSelector(state => state.posts.status)
-
-  useEffect(() => {
-    if (postStatus === 'idle') {
-      dispatch(fetchPosts())
-    }
-  }, [postStatus, dispatch])
-  
-  const renderedPosts = posts.map(post => (
+const PostExcerpt = ({ post }) => {
+  return (
     <article className='post-excerpt' key={post.id}>
       <h3>{post.title}</h3>
       <PostAuthor userId={post.user} />
@@ -31,12 +21,42 @@ export const PostsList = () => {
         View Post
       </Link>
     </article>
-  ))
+  )
+}
+
+export const PostsList = () => {
+  const dispatch = useDispatch()
+  const posts = useSelector(selectAllPosts)
+
+  const postStatus = useSelector(state => state.posts.status)
+  const error = useSelector(state => state.posts.error)
+
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts())
+    }
+  }, [postStatus, dispatch])
+
+  let content
+
+  if ( postStatus === 'loading') {
+    content = <Spinner text='Loading...' />
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts
+    .slice()
+    .sort((a,b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map(post => (
+      <PostExcerpt key={post.id} post={post} />
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
